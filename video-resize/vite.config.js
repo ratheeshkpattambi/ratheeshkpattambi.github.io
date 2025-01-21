9d98f702be02606b71fe111c6910a46fc064585d
Ratheesh Kalarot <ratheeshkpattambi@gmail.com>
2025-01-20 20:16:26 -0800

    Restructure

--- video-resize/vite.config.js
+++ video-resize/vite.config.js
@@ -4,7 +4,7 @@ export default defineConfig({
   root: 'src', // Set the source directory
   build: {
     outDir: '../', // Output files directly into `tools/video-resize`
-    emptyOutDir: true, // Clean the folder before building
+    emptyOutDir: false, // Clean the folder before building
   },
   server: {
     headers: {
