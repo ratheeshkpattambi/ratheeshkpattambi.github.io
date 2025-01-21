72cf5264499818f04a80001d5688638c6b86083e
Ratheesh Kalarot <ratheeshkpattambi@gmail.com>
2025-01-20 20:30:02 -0800

    minor

--- video-resize/vite.config.js
+++ video-resize/vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
   root: 'src', // Set the source directory
+  base: './', // Ensures relative paths for assets
   build: {
     outDir: '../', // Output files directly into `tools/video-resize`
     emptyOutDir: false, // Clean the folder before building
